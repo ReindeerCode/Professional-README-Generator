24e1b897caa179f7376e5c6cf847108c3ed9a913
MJRudolph <ReindeerCode@gmail.com>
2020-11-17 18:31:15 -0500

    Fixed ignore file location and added Type to package.json

---
+++ index.js
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+import fs from "fs";
+
+// inquirer
+//   .prompt([
+//     {
+//       type: "confirm",
+//       name: "toBeDelivered",
+//       message: "Is this for delivery?",
+//       default: false,
+//     },
+//   ])
+//   .then((answers) => {
+//     console.log(answers);
+//   });
+
+// array of questions for user
+const questions = [];
+
+// function to write README file
+function writeToFile(fileName, data) {}
+
+// function to initialize program
+function init() {}
+
+// function call to initialize program
+init();
