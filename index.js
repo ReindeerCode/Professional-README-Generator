a6c7451cf2c81fca44d9ae81f3675607432cf9ac
MJRudolph <ReindeerCode@gmail.com>
2020-11-18 18:22:14 -0500

    Questions added

--- index.js
+++ index.js
@@ -1,21 +1,56 @@
 import inquirer from "inquirer";
 import fs from "fs";
+import util from "util";
 
 // inquirer
-//   .prompt([
-//     {
-//       type: "confirm",
-//       name: "toBeDelivered",
-//       message: "Is this for delivery?",
-//       default: false,
-//     },
-//   ])
+//   .prompt(questions)
 //   .then((answers) => {
 //     console.log(answers);
 //   });
 
 // array of questions for user
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "Name",
+    message: "What is your name?",
+  },
+  {
+    type: "input",
+    name: "Description",
+    message: "What is your project description",
+  },
+  {
+    type: "input",
+    name: "Installation Instructions",
+    message: "What are the installation instructions?",
+  },
+  {
+    type: "input",
+    name: "Usage Information",
+    message: "Provide usage information for the user.",
+  },
+  {
+    type: "input",
+    name: "Usage Information",
+    message: "Provide usage information for the user.",
+  },
+  {
+    type: "input",
+    name: "Usage Information",
+    message: "Provide usage information for the user.",
+  },
+  {
+    type: "input",
+    name: "Contribution Guidelines",
+    message: "Outline the contribution guidelines.",
+  },
+  {
+    type: "input",
+    name: "Test Instructions",
+    message: "Outline the contribution guidelines.",
+  },
+];
 
 // function to write README file
 function writeToFile(fileName, data) {}
