4e8358ad51123d6a472e612ac30f004cbb3e5a87
MJRudolph <ReindeerCode@gmail.com>
2020-11-19 17:36:30 -0500

    Testing Output Format

--- README.md
+++ README.md
@@ -7,68 +7,68 @@
         rel="stylesheet"
         href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
       />
-      <title>Document</title>
     </head>
     <body>
       <div>
-          <h1 class="display-4">### Project:</h1>
-          <h3> Test 1</h3>
+          <h1 class="display-4">Project:</h1>
+          <h4> Test 1</h4>
       </div>
       </br>
       <div>
-      <h2>## Table of Contents: </h2> 
+      <h2> Table of Contents: </h2> 
       <ul>
-          <li>* [Name](#name) </li>
-          <li>* [License](#license) </li>
-          <li>* [Description](#description) </li>
-          <li>* [Installation Instructions](#installationInstructions) </li>
-          <li>* [Test](#test) </li>
-          <li>* [GitHub](#gitHub) </li>
-          <li>* [Email](#email) </li>
-          <li>* [Contributors](#contributors) </li>
+          <li>*[Name](#name) </li>
+          <li>*[License](#license) </li>
+          <li>*[Description](#description) </li>
+          <li>*[Installation Instructions](#installationInstructions) </li>
+          <li>*[Test](#test) </li>
+          <li>*[GitHub](#gitHub) </li>
+          <li>*[Email](#email) </li>
+          <li>*[Contributors](#contributors) </li>
       </ul>
       </div>
   </br>
       <div>
-          <h2>## License: </h2>
-          <h3>### MIT </h3>
+          <h2>License: </h2>
+          <h4>MIT </h4>
       </div>
       </br>   
       <div>
-          <h2>## Description: </h2>
-          <h3>### Test2 </h3>
+          <h2>Description: </h2>
+          <h4>Test 2 </h4>
       </div>
       </br>   
       <div>
-      <h2>### Images: </h2> 
+      <h2>Images: </h2> 
           <ul>
-              <li>* ![Project Screenshot](./imagePathHere.png) </li>
-              <li>* ![Project Screenshot](./imagePathHere.png) </li>
-              <li>* ![Project Screenshot](./imagePathHere.png) </li>
+              <li>![Project Screenshot](./imagePathHere.png) </li>
+              <li>![Project Screenshot](./imagePathHere.png) </li>
+              <li>![Project Screenshot](./imagePathHere.png) </li>
           </ul>
       </div>
       <div>
-          <h2>## Installation Instructions: </h2>
-          <h3>### Test3</h3>
+          <h2>Installation Instructions: </h2>
+          <h4>Test 3</h4>
       </div>
       </br> 
       <div>
-          <h2>## Test Command:  </h2>
-          <h3>### To test type Test4 into the terminal and follow prompts</h3>
+          <h2>Test Command:</h2>
+          <h4>To test type Test 4 into the terminal and follow prompts</h4>
       </div>
+      </br>
       <div>
-          <h2>## Other Contributors:  </h2>
-          <h3>### Test7</h3>
-      </div>
-      </br> 
-      <div>
-          <h2>## My Github Username:   </h2>
-          <h3>### Check out more project on my Github at Test5</h3>
+          <h2>Other Contributors:</h2>
+          <h4>Test 7</h4>
       </div>
       </br> 
       <div>
-          <h2>## My Email Address:  </h2>
-          <h3>### If you have any question please feel free to email me at Test6</h3>
+          <h2>My Github Username:   </h2>
+          <h4>Check out more project on my Github at Test 5</h4>
+      </div>
+      </br> 
+      <div>
+          <h2>My Email Address:  </h2>
+          <h4>If you have any question please feel free to email me at Test 6</h4>
       </div>
     </body>
   </html>
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -9,68 +9,68 @@ function generateMarkdown(answers) {
         rel="stylesheet"
         href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
       />
-      <title>Document</title>
     </head>
     <body>
       <div>
-          <h1 class="display-4">### Project:</h1>
-          <h3> ${answers.name}</h3>
+          <h1 class="display-4">Project:</h1>
+          <h4> ${answers.name}</h4>
       </div>
       </br>
       <div>
-      <h2>## Table of Contents: </h2> 
+      <h2> Table of Contents: </h2> 
       <ul>
-          <li>* [Name](#name) </li>
-          <li>* [License](#license) </li>
-          <li>* [Description](#description) </li>
-          <li>* [Installation Instructions](#installationInstructions) </li>
-          <li>* [Test](#test) </li>
-          <li>* [GitHub](#gitHub) </li>
-          <li>* [Email](#email) </li>
-          <li>* [Contributors](#contributors) </li>
+          <li>*[Name](#name) </li>
+          <li>*[License](#license) </li>
+          <li>*[Description](#description) </li>
+          <li>*[Installation Instructions](#installationInstructions) </li>
+          <li>*[Test](#test) </li>
+          <li>*[GitHub](#gitHub) </li>
+          <li>*[Email](#email) </li>
+          <li>*[Contributors](#contributors) </li>
       </ul>
       </div>
   </br>
       <div>
-          <h2>## License: </h2>
-          <h3>### ${answers.license} </h3>
+          <h2>License: </h2>
+          <h4>${answers.license} </h4>
       </div>
       </br>   
       <div>
-          <h2>## Description: </h2>
-          <h3>### ${answers.description} </h3>
+          <h2>Description: </h2>
+          <h4>${answers.description} </h4>
       </div>
       </br>   
       <div>
-      <h2>### Images: </h2> 
+      <h2>Images: </h2> 
           <ul>
-              <li>* ![Project Screenshot](./imagePathHere.png) </li>
-              <li>* ![Project Screenshot](./imagePathHere.png) </li>
-              <li>* ![Project Screenshot](./imagePathHere.png) </li>
+              <li>![Project Screenshot](./imagePathHere.png) </li>
+              <li>![Project Screenshot](./imagePathHere.png) </li>
+              <li>![Project Screenshot](./imagePathHere.png) </li>
           </ul>
       </div>
       <div>
-          <h2>## Installation Instructions: </h2>
-          <h3>### ${answers.installationInstructions}</h3>
+          <h2>Installation Instructions: </h2>
+          <h4>${answers.installationInstructions}</h4>
       </div>
       </br> 
       <div>
-          <h2>## Test Command:  </h2>
-          <h3>### To test type ${answers.test} into the terminal and follow prompts</h3>
+          <h2>Test Command:</h2>
+          <h4>To test type ${answers.test} into the terminal and follow prompts</h4>
       </div>
+      </br>
       <div>
-          <h2>## Other Contributors:  </h2>
-          <h3>### ${answers.contributors}</h3>
-      </div>
-      </br> 
-      <div>
-          <h2>## My Github Username:   </h2>
-          <h3>### Check out more project on my Github at ${answers.gitHub}</h3>
+          <h2>Other Contributors:</h2>
+          <h4>${answers.contributors}</h4>
       </div>
       </br> 
       <div>
-          <h2>## My Email Address:  </h2>
-          <h3>### If you have any question please feel free to email me at ${answers.email}</h3>
+          <h2>My Github Username:   </h2>
+          <h4>Check out more project on my Github at ${answers.gitHub}</h4>
+      </div>
+      </br> 
+      <div>
+          <h2>My Email Address:  </h2>
+          <h4>If you have any question please feel free to email me at ${answers.email}</h4>
       </div>
     </body>
   </html>
